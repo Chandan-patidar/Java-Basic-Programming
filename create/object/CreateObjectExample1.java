5925cd2542354bc72d2d6c30baa295cf820d6cbf
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2024-08-25 14:18:10 +0530

    Creating object with new keyword

---
+++ create/object/CreateObjectExample1.java
@@ -0,0 +1,14 @@
+package create.object;
+
+public class CreateObjectExample1 {
+    void show()
+    {
+        System.out.println("Welcome to java with chandan");
+    }
+    public static void main(String[] args){
+        //Creating an object using new Keyword
+        CreateObjectExample1 obj = new CreateObjectExample1();
+        //invoking  method using object
+        obj.show();
+    }
+}

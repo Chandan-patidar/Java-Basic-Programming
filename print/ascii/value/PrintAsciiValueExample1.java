1f27e040f42d8c93588e882a599dbb926657ac35
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2024-08-25 11:48:54 +0530

    program of ascii ki value

---
+++ print/ascii/value/PrintAsciiValueExample1.java
@@ -0,0 +1,17 @@
+package print.ascii.value;
+
+public class PrintAsciiValueExample1 {
+    public  static void main(String args[]){
+        // character whose ASCII value to be found
+        char ch1 ='A';
+        char ch2 ='b';
+        // variable that store the integer value of the  character
+        int asciivalue1 = ch1;
+        int asccivalue2 = ch2;
+        System.out.println("The ASCII value " +ch1+" is :"+ asciivalue1);
+        System.out.println("The ASCII value " +ch1+" is :"+ asccivalue2);
+
+
+
+    }
+}

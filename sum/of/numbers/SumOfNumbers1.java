daf8e4160de38e50625e8845a11bbd51b6407aed
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2024-08-28 00:37:14 +0530

    sum of two number

---
+++ sum/of/numbers/SumOfNumbers1.java
@@ -0,0 +1,9 @@
+package sum.of.numbers;
+
+public class SumOfNumbers1 {
+    public static void main(String args[]){
+        int n1=225,n2=23,sum;
+        sum=n1+n2;
+        System.out.println("The sum of number is : " +sum);
+    }
+}

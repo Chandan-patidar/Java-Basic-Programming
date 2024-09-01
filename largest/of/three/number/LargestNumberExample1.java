0598fcd050286ea0c446cbb97ff0542bf81bb8ff
Chandan Patidar <88586932+Chandan-patidar@users.noreply.github.com>
2024-09-01 12:33:22 +0530

    largest among 3 number

--- largest/of/three/number/LargestNumberExample1.java
+++ largest/of/three/number/LargestNumberExample1.java
@@ -1,4 +1,24 @@
 package largest.of.three.number;
 
+import java.util.Scanner;
+
 public class LargestNumberExample1 {
+    public static void main (String[] args){
+    int a, b, c, largest, temp;
+    // object of the Scanner class
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first numbe:");
+    a=sc.nextInt();
+    System.out.println("Enter the second number ");
+    b=sc.nextInt();
+    System.out.println("Enter the third number ");
+    c=sc.nextInt();
+    // comparing a and b and strong the largest number in a temp variable
+    temp=a>b?a:b;
+    // Comparing temp variable with c and storing the result in the variable
+    largest =c >temp?c:temp;
+    //print the largest number
+    System.out.println("The largest number is : "+largest);
+}
+
 }
